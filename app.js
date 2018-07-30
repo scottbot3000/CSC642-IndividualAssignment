1272cde5bf3e92cfb271d6181606582231c6e684
scottbot3000 <scott.carbonbased@gmail.com>
2018-07-29 22:18:38 -0700

    Added baseline form skeleton with Bootstrap, and added captcha support

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 const express = require('express')
 const path = require('path')
+const session = require('express-session')
+var svgCaptcha = require('svg-captcha');
+
 const PORT = process.env.PORT || 5000
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+	.use(session({
+		secret: 'csc648-stock-overflow',
+		resave: false,
+		saveUninitialized: true
+	}))
+	.use(express.static(path.join(__dirname, 'public')))
+	.set('views', path.join(__dirname, 'views'))
+	.set('view engine', 'ejs')
+	.get('/', function(req, res) {
+		var captcha = svgCaptcha.create({color: true});
+		req.session.captcha = captcha.text;
+		console.log(captcha.text)
+		res.render('pages/index', {
+			data: captcha.data
+		})
+	})
+	.get('/results', (req, res) => res.render('pages/results'))
+	.listen(PORT, () => console.log(`Listening on ${ PORT }`))
